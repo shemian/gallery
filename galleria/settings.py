8cf23ac0775ba78530911ce8eb3e947e2c04415d
ian <shemian092@gmail.com>
2020-08-03 13:59:45 +0300

    dep

--- galleria/settings.py
+++ galleria/settings.py
@@ -41,6 +41,7 @@ else:
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
