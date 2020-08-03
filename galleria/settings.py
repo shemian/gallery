58aa9976baba5e16c5860b5b02e3849611ac2e87
ian <shemian092@gmail.com>
2020-08-03 13:57:47 +0300

    dep

--- galleria/settings.py
+++ galleria/settings.py
@@ -165,3 +165,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
